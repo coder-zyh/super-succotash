2041bd1733eb55839d181946e27235f638214028
unknown <yuhang.zhang@fintecher.cn>
2022-07-21 18:05:00 +0800

    fix: test2

--- src/pages/main/fee/FeeFill.vue
+++ src/pages/main/fee/FeeFill.vue
@@ -3,73 +3,33 @@
 		<head-top>
 			<template #left><span @click="cancel">取消</span></template>
 			报销填报
-			<template #right><span @click="save">保存</span></template>
+			<template #right><span @click="save">提交</span></template>
 		</head-top>
-		<van-cell-group>
-			<van-field
-				v-model="project1"
-				is-link
-				readonly
-				label="报销项目"
-				placeholder="选择项目名称"
-				@click="showPicker1 = true"
-			/>
-			<van-popup v-model:show="showPicker1" round position="bottom">
-				<van-picker
-					:columns="columns1"
-					input-align="right"
-					@cancel="showPicker1 = false"
-					@confirm="onConfirm1"
-				/>
-			</van-popup>
-			<van-field
-				v-model="project2"
-				is-link
-				readonly
-				label="报销类型"
-				placeholder="选择报销类型"
-				@click="showPicker2 = true"
-			/>
-			<van-popup v-model:show="showPicker2" round position="bottom">
-				<van-picker
-					:columns="columns2"
-					input-align="right"
-					@cancel="showPicker2 = false"
-					@confirm="onConfirm2"
-				/>
-			</van-popup>
-			<van-cell title="选择单个日期" :value="date" @click="show = true" />
-			<van-calendar
-				v-model:show="show"
-				:min-date="minDate"
-				:max-date="maxDate"
-				:show-confirm="false"
-				@confirm="onConfirm"
-			/>
-			<van-field
-				v-model="money"
-				label="报销金额"
-				placeholder="请输入"
-				input-align="right"
-			/>
-		</van-cell-group>
+		<fill-item
+			v-for="(item, index) in list"
+			:key="index"
+			ref="box"
+			:index="index"
+		></fill-item>
+		<div class="btn">
+			<van-button type="success" @click="createNew">点击添加</van-button>
+			<van-button type="danger" @click="remove">点击删除</van-button>
+		</div>
 	</div>
 </template>
 <script>
 import HeadTop from "@/components/HeadTop.vue";
+import FillItem from "./children/FillItem.vue";
 import { useRouter } from "vue-router";
-
 import { ref } from "vue";
+import { CreateFeeForm } from "@/utils/class.js";
 
 export default {
 	components: {
 		HeadTop,
+		FillItem,
 	},
 	setup() {
-		const app = {
-			name: "myte",
-			type: "report",
-		};
 		// 导入路由
 		const router = useRouter();
 		// 点击取消回到报销页面
@@ -77,69 +37,70 @@ export default {
 			router.back();
 		};
 		// 点击保存调用接口
+		// 获取元素
+		let box = ref(null);
 		const save = () => {
-			console.log("save");
+			// 创建报销列表,添加数据到报销列表
+			const fillForm = pushData();
+			console.log(fillForm);
+			// 发送post请求
+			postList(fillForm);
+			// 跳转至fee页面
+			router.replace("/fee");
 		};
-		//项目选择
-		//调用本地存储的可选项目列表
-		const columns1 = JSON.parse(localStorage.getItem("projectList"));
-		const project1 = ref("");
-		const showPicker1 = ref(false);
-		const onConfirm1 = (value) => {
-			project1.value = value;
-			showPicker1.value = false;
+		const list = ref([{}]);
+		const pushData = () => {
+			const arr = [];
+			box.value.forEach((item) => {
+				const obj = new CreateFeeForm(
+					item.project.split("-")[1],
+					item.project.split("-")[0],
+					item.feeType,
+					item.date,
+					item.money
+				);
+				arr.push(obj);
+			});
+			return arr;
 		};
-		//调用本地存储的可选项目列表
-		const columns2 = [
-			"日常报销-业务招待",
-			"日常报销-市内交通",
-			"日常报销-团建费",
-			"日常报销-参会及培训费",
-			"日常报销-其他",
-			"付款申请",
-			"差旅报销",
-		];
-		const project2 = ref("");
-		const showPicker2 = ref(false);
-		const onConfirm2 = (value) => {
-			project2.value = value;
-			showPicker2.value = false;
+		const postList = () => {
+			new Promise((resolve) => {
+				setTimeout(() => {
+					resolve("success-post");
+				}, 2000);
+			}).then((value) => {
+				console.log(value);
+			});
 		};
-		// 日期选择
-		const date = ref("");
-		const show = ref(false);
+		// 添加移除项目函数;
+		const createNew = () => {
+			list.value.push({});
+		};
+		const remove = () => {
+			list.value.pop();
+		};
 
-		const formatDate = (date) => `${date.getMonth() + 1}/${date.getDate()}`;
-		const onConfirm = (value) => {
-			show.value = false;
-			date.value = formatDate(value);
-		};
-		// 金额选择
-		const money = 0;
 		return {
-			app,
 			cancel,
 			save,
-			project1,
-			showPicker1,
-			onConfirm1,
-			columns1,
-			project2,
-			showPicker2,
-			onConfirm2,
-			columns2,
-			date,
-			show,
-			onConfirm,
-			minDate: new Date(2022, 7, 1),
-			maxDate: new Date(2022, 7, 31),
-			money,
+			list,
+			createNew,
+			remove,
+			box,
+			pushData,
 		};
 	},
 };
 </script>
 <style scoped>
+.fill {
+	min-height: 85vh;
+}
 .fill span {
 	color: rgb(33, 33, 239);
+}
+.btn {
+	display: flex;
+	justify-content: space-around;
 }
 </style>
--- src/pages/main/fee/children/FeeItem.vue
+++ src/pages/main/fee/children/FeeItem.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="feeItem">
 		<van-cell-group v-if="item" inset>
-			<van-cell :value="item.name" border="false" />
-			<van-cell title="费用时间" :value="item.date" border="false" />
-			<van-cell title="报销金额" :value="item.money" border="false" />
-			<van-cell title="报销科目" :value="item.feeType" border="false" />
-			<van-cell title="报销时间" :value="item.date" border="false" />
+			<van-cell :value="item.name" />
+			<van-cell title="费用时间" :value="item.date" />
+			<van-cell title="报销金额" :value="item.money" />
+			<van-cell title="报销科目" :value="item.feeType" />
+			<van-cell title="报销时间" :value="item.date" />
 		</van-cell-group>
 	</div>
 </template>
